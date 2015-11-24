af9034551d278b5f91f763ba6604da101ec0bae8
TonyChyi <tonychee1989@gmail.com>
2015-11-24 13:51:12 +0800

    js'

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("div.button#kaka").click(function(){
+        window.location.href="https://kaka.wetofu.top";
+    });
+    $("div.button#tonychyi").click(function(){
+        window.location.href="https://tonychyi.wetofu.top";
+    });
+});
